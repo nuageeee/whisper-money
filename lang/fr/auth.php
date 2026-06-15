cd002716fbff2b3121b466cc3fcc4c3aa1301689
elnuage <joachinloison@outlook.fr>
2026-06-15 10:47:39 +0200

    Add .php french translations

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Aucun compte enregistré avec les identifiants fournis.',
+    'password' => 'Le mot de passe est incorect.',
+    'throttle' => 'Trop de tentatives. Veuillez ressayer dans :seconds secondes.',
 
 ];
